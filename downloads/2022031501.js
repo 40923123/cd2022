827c877d7c3eb788ca80c3ddf7ccbae8da22d347
40923123 <40923123@gm.nfu.edu.tw>
2022-03-15 20:17:56 +0800

    W3 update01

---
+++ downloads/2022031501.js
@@ -0,0 +1,20 @@
+﻿var winkVideoData = {
+  dataVersion: 1,
+  frameRate: 10,
+  buttonFrameLength: 5,
+  buttonFrameOffset: 2,
+  frameStops: {
+    "2": [
+      { type: "gotoframe", rect: { x: 528, y: 533, width: 60, height: 24 }, target: 3 },
+    ],
+    "93": [
+      { type: "gotoframe", rect: { x: 494, y: 207, width: 60, height: 24 }, target: 94 },
+    ],
+    "159": [
+      { type: "gotoframe", rect: { x: 538, y: 291, width: 60, height: 24 }, target: 160 },
+    ],
+    "185": [
+      { type: "gotoframe", rect: { x: 312, y: 224, width: 60, height: 24 }, target: 186 },
+    ],
+  },
+};

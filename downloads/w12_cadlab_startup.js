9a8a8423ac7c96fc6972346b9c8f86576c84a0ad
40923123 <40923123@gm.nfu.edu.tw>
2022-05-11 11:18:25 +0800

    w12_cadlab_startup frist update

---
+++ downloads/w12_cadlab_startup.js
@@ -0,0 +1,32 @@
+﻿var winkVideoData = {
+  dataVersion: 1,
+  frameRate: 10,
+  buttonFrameLength: 5,
+  buttonFrameOffset: 2,
+  frameStops: {
+    "47": [
+      { type: "gotoframe", rect: { x: 901, y: 523, width: 60, height: 24 }, target: 48 },
+      { type: "gotoframe", rect: { x: 830, y: 524, width: 60, height: 24 }, target: 0 },
+    ],
+    "66": [
+      { type: "gotoframe", rect: { x: 523, y: 398, width: 60, height: 24 }, target: 67 },
+      { type: "gotoframe", rect: { x: 454, y: 398, width: 60, height: 24 }, target: 47 },
+    ],
+    "77": [
+      { type: "gotoframe", rect: { x: 277, y: 481, width: 60, height: 24 }, target: 78 },
+      { type: "gotoframe", rect: { x: 208, y: 481, width: 60, height: 24 }, target: 66 },
+    ],
+    "130": [
+      { type: "gotoframe", rect: { x: 776, y: 178, width: 60, height: 24 }, target: 131 },
+      { type: "gotoframe", rect: { x: 702, y: 177, width: 60, height: 24 }, target: 77 },
+    ],
+    "154": [
+      { type: "gotoframe", rect: { x: 870, y: 297, width: 60, height: 24 }, target: 155 },
+      { type: "gotoframe", rect: { x: 806, y: 298, width: 60, height: 24 }, target: 130 },
+    ],
+    "200": [
+      { type: "gotoframe", rect: { x: 575, y: 264, width: 60, height: 24 }, target: 201 },
+      { type: "gotoframe", rect: { x: 504, y: 263, width: 60, height: 24 }, target: 154 },
+    ],
+  },
+};

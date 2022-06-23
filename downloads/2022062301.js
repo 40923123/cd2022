6707b12c089c1de75f01b7f0d300b0e37295908f
40923123 <40923123@gm.nfu.edu.tw>
2022-06-23 22:58:16 +0800

    任務二第一部wink

---
+++ downloads/2022062301.js
@@ -0,0 +1,22 @@
+﻿var winkVideoData = {
+  dataVersion: 1,
+  frameRate: 10,
+  buttonFrameLength: 5,
+  buttonFrameOffset: 2,
+  frameStops: {
+    "2": [
+      { type: "gotoframe", rect: { x: 462, y: 229, width: 60, height: 24 }, target: 3 },
+    ],
+    "9": [
+      { type: "gotoframe", rect: { x: 461, y: 189, width: 60, height: 24 }, target: 10 },
+      { type: "gotoframe", rect: { x: 392, y: 189, width: 60, height: 24 }, target: 2 },
+    ],
+    "16": [
+      { type: "gotoframe", rect: { x: 392, y: 189, width: 60, height: 24 }, target: 9 },
+      { type: "gotoframe", rect: { x: 461, y: 189, width: 60, height: 24 }, target: 17 },
+    ],
+    "21": [
+      { type: "gotoframe", rect: { x: 392, y: 189, width: 60, height: 24 }, target: 16 },
+    ],
+  },
+};

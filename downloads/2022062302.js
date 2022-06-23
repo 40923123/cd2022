36d83f62be96c698251505029ef815944bd301ec
40923123 <40923123@gm.nfu.edu.tw>
2022-06-23 23:24:27 +0800

    任務二第二部wink

---
+++ downloads/2022062302.js
@@ -0,0 +1,30 @@
+﻿var winkVideoData = {
+  dataVersion: 1,
+  frameRate: 10,
+  buttonFrameLength: 5,
+  buttonFrameOffset: 2,
+  frameStops: {
+    "2": [
+      { type: "gotoframe", rect: { x: 538, y: 329, width: 60, height: 24 }, target: 3 },
+    ],
+    "7": [
+      { type: "gotoframe", rect: { x: 1067, y: 406, width: 60, height: 24 }, target: 8 },
+      { type: "gotoframe", rect: { x: 996, y: 407, width: 60, height: 24 }, target: 2 },
+    ],
+    "48": [
+      { type: "gotoframe", rect: { x: 1002, y: 372, width: 60, height: 24 }, target: 49 },
+      { type: "gotoframe", rect: { x: 934, y: 371, width: 60, height: 24 }, target: 7 },
+    ],
+    "63": [
+      { type: "gotoframe", rect: { x: 476, y: 198, width: 60, height: 24 }, target: 64 },
+      { type: "gotoframe", rect: { x: 406, y: 198, width: 60, height: 24 }, target: 48 },
+    ],
+    "95": [
+      { type: "gotoframe", rect: { x: 1029, y: 397, width: 60, height: 24 }, target: 96 },
+      { type: "gotoframe", rect: { x: 960, y: 396, width: 60, height: 24 }, target: 63 },
+    ],
+    "100": [
+      { type: "gotoframe", rect: { x: 616, y: 229, width: 60, height: 24 }, target: 95 },
+    ],
+  },
+};
